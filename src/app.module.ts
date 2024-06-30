f1ea2222b19b32f02621cd2d516662c726ab9345
Muhammad Bilal Bangash <muhammadbilalbangash@hotmail.com>
2024-06-30 16:20:10 +0500

    crud resolver in progess

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { BookModule } from './book/book.module';
 import { join } from 'path';
 import { AppResolver } from './app.resolver';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { AppResolver } from './app.resolver';
       // typePaths: ['./**/*.graphql'],
     }),
     BookModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/graphql.ts
+++ src/graphql.ts
@@ -8,15 +8,35 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export interface CreateUserInput {
+    exampleField: number;
+}
+
+export interface UpdateUserInput {
+    exampleField?: Nullable<number>;
+    id: number;
+}
+
 export interface Book {
     id: number;
     title: string;
     price?: Nullable<number>;
 }
 
+export interface User {
+    exampleField: number;
+}
+
 export interface IQuery {
     index(): string | Promise<string>;
     books(): Nullable<Book[]> | Promise<Nullable<Book[]>>;
+    user(id: number): User | Promise<User>;
+}
+
+export interface IMutation {
+    createUser(createUserInput: CreateUserInput): User | Promise<User>;
+    updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+    removeUser(id: number): User | Promise<User>;
 }
 
 type Nullable<T> = T | null;
